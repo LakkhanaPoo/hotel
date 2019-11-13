9dadfcd04ee709d9e65198ff3b11277bed49e2c1
lakkhanaPoo <lakkhana.poo@truedigital.com>
2019-11-14 00:45:14 +0700

    remove unuse file

--- src/components/Thumbnail.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Thumbnail = props => {
-  const { farm, server, id, secret } = props.photo; // destructuring
-
-  return <img src={props.photo} alt={props.photo} />;
-};
-
-export default Thumbnail;
